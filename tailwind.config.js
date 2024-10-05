eb2d19ec83f97094ac4da24a539861e275582342
alimur16 <murtadhoali22@gmail.com>
2024-10-05 20:27:04 +0700

    feat : Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ const flowbite = require('flowbite-react/tailwind');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['InterVariable', 'sans-serif'],
+      },
+    },
   },
   plugins: [flowbite.plugin()],
 };
